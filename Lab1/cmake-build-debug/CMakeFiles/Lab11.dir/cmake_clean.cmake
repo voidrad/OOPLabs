4cb8a536aa547f739558a916653defc29a3ef34a
voidrad <54220741+voidrad@users.noreply.github.com>
2021-09-01 01:11:00 +0300

    ReadySovsem

---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab11.dir/Lab1.cpp.obj"
+  "Lab11.exe"
+  "Lab11.exe.manifest"
+  "Lab11.pdb"
+  "libLab11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
